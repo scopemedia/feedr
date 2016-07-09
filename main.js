23559ba444e692dbcaec75ce37175eb9ac3401c2
scopemedia <ewan@scopemedia.com.au>
2016-07-09 23:22:15 +1000

    Start of RSS switcher

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 
   var container = document.querySelector('#container')
   var state = {loading: true}
-
+  var feedOrigin = '';
   // data.items.item.img
   var data = []
 
@@ -46,53 +46,127 @@
       // update state
       state = {loading: false}
 
-      var feed = dataAsJson.data.children;
-
       // Render Article Template
 
-      function renderImage(item) {
+      if (url === 'https://crossorigin.me/http://mashable.com/stories.json') {
+        console.log('Mashable');
 
-        // console.log('Image - ' + item.data.preview.images[0].resolutions[0].url);
+        var feed = dataAsJson.hot;
 
-          return `
-            <section class="featured-image">
-              no image
+        function renderImage(item) {
+
+            return `
+              <section class="featured-image">
+                no image
+              </section>
+              <section class="article-content">
+                <a href="${item.link}"><h3>${item.title}</h3></a>
+                <h6>${item.channel}</h6>
+              </section>
+              <section class="impressions">
+                ${item.shares.total}
+              </section>
+              <div class="clearfix"></div>
+            `
+        }
+
+
+        // Render Section Module Template
+
+        function renderArticles(data, into) {
+
+          into.innerHTML = `
+            <section id="main" class="wrapper">
+              ${data.map((item) => {
+
+                // if (item.data.preview.images) {
+                //   console.log('yes to image')
+                // }
+                return `
+                  <article class="article">
+                    ${renderImage(item)}
+                    <div class="clearfix"></div>
+                  </article>
+                `
+              })}
             </section>
-            <section class="article-content">
-              <a href="${item.data.url}"><h3>${item.data.title}</h3></a>
-              <h6>${item.data.subreddit}</h6>
-            </section>
-            <section class="impressions">
-              ${item.data.ups}
-            </section>
-            <div class="clearfix"></div>
           `
+        }
+
+        renderArticles(feed, container);
+
+      } else if (url === 'https://www.reddit.com/top.json') {
+        console.log('Reddit');
+        var feed = dataAsJson.data.children;
+
+        function renderImage(item) {
+
+          // console.log('Image - ' + item.data.preview.images[0].resolutions[0].url);
+
+            return `
+              <section class="featured-image">
+                no image
+              </section>
+              <section class="article-content">
+                <a href="${item.data.url}"><h3>${item.data.title}</h3></a>
+                <h6>${item.data.subreddit}</h6>
+              </section>
+              <section class="impressions">
+                ${item.data.ups}
+              </section>
+              <div class="clearfix"></div>
+            `
+        }
+
+
+        // Render Section Module Template
+
+        function renderArticles(data, into) {
+
+          into.innerHTML = `
+            <section id="main" class="wrapper">
+              ${data.map((item) => {
+
+                // if (item.data.preview.images) {
+                //   console.log('yes to image')
+                // }
+                return `
+                  <article class="article">
+                    ${renderImage(item)}
+                    <div class="clearfix"></div>
+                  </article>
+                `
+              })}
+            </section>
+          `
+        }
+
+        renderArticles(feed, container);
+
       }
 
 
-      // Render Section Module Template
 
-      function renderArticles(data, into) {
+      // Render Popup
+
+      function popUp(data, into) {
 
         into.innerHTML = `
-          <section id="main" class="wrapper">
-            ${data.map((item) => {
-              // console.log('date map - ' + item.data.preview.images[0].resolutions[0].url);
-              return `
-                <article class="article">
-                  ${renderImage(item)}
-                  <div class="clearfix"></div>
-                </article>
-              `
-            })}
-          </section>
+          <div id="pop-up">
+            <a href="#" class="close-pop-up">X</a>
+            <div class="wrapper">
+              <h1>Article title here</h1>
+              <p>
+              Article description/content here.
+              </p>
+              <a href="#" class="pop-up-action" target="_blank">Read more from source</a>
+            </div>
+          </div>
         `
       }
 
 
-
-
-      renderArticles(feed, container);
+      // popUp(feed, popup);
 
     }).catch(function(err) {
       console.log('Error', err);
@@ -104,11 +178,36 @@
   // Call the loadRSS function with desired RSS feed
 
   loadRSS('https://www.reddit.com/top.json')
+  // loadRSS('https://crossorigin.me/http://mashable.com/stories.json')
 
 
   // listener for click
   // render state
   // pop-up
+
+//  If nav li a content is 'Reddit' then
+//  loadRSS('https://www.reddit.com/top.json')
+
+
+
+var switcher = document.querySelectorAll('nav li ul li a');
+for (var i = 0; i < switcher.length; i++) {
+
+  // Function that checks text content of link and loads matching RSS feed
+  function clickMe() {
+    if (this.textContent === 'Reddit') {
+      loadRSS('https://www.reddit.com/top.json')
+    } else if (this.textContent === 'Mashable') {
+      loadRSS('https://crossorigin.me/http://mashable.com/stories.json')
+    }
+  }
+
+  // Listens for clicks on links and calls function to switch RSS feed
+  switcher[i].addEventListener("click", clickMe, false);
+
+}
+//
+
 
 
 })()
